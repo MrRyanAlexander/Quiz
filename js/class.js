ff77377cf8b5d87cacd920f123e315e7dbcebe3c
MrRyanAlexander <lazyguymedia@gmail.com>
2013-10-06 16:24:06 -0700

    updated questions

--- js/class.js
+++ js/class.js
@@ -1,136 +1,77 @@
-//$(document).ready(function() {
-//  $(".see-photos").on("click", function(event) {
-//    event.preventDefault();
-//    $(this).closest(".tour").find(".photos").slideToggle();
-//  });
-//  $(".photos").on("mouseenter", "li", function(){
-//  	//$(this).closest(".tour").find("span").fadeUp();
-//  	$(this).find("span").slideToggle();
 /*!
  * Dynamic jQuery Quiz
  * http://github.com/MrRyanAlexander/Quiz
  *
- * @updated September 23, 2013
+ * @updated October 6, 2013
  *
  * @author Ryan Alexander - http://www.ryan-alexander.com
  * @license MIT
  */
 
-/*!start = start quiz
-*  start = hide start button
-*  start = get quiz question
-*  start = show first unanswered question 
-*  
-*  [ main quiz ]
-*  
-*  next = get current selection
-*  next = match selection to correct answer
-*  next = store answer
-*  next = update question to answered to prevent it from being asked again
-*  next = get next answered question
-*  next = go to next question
-*  
-*  if next cannot find an unaswered question we have reached the end
-*  hide quiz and show results
-*  show button to restart quiz
-*/ 
+    
+
+$(document).ready(function () {
+
+    //create log 
     function log(t){
         console.log(t);
     }
 
-	var q = [
-	 	{
-    	question: "1Who is Prime Minister of the United Kingdom?", 
-    	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-    	correctAnswer: "David Cameron",
-    	storedAnswer:"",
-    	answered:false},
-    	{
-    	question: "2Who is Prime Minister of the United Kingdom?", 
-    	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-    	correctAnswer:"Gordon Brown",
-    	storedAnswer:"",
-    	answered:false},
-    	{
-    	question: "3Who is Prime Minister of the United Kingdom?", 
-    	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-    	correctAnswer:"Gordon Brown",
-    	storedAnswer:"",
-    	answered:false},
-    	{
-    	question: "4Who is Prime Minister of the United Kingdom?", 
-    	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-    	correctAnswer:"David Cameron",
-    	storedAnswer:"",
-    	answered:false},
-    	{
-    	question: "5Who is Prime Minister of the United Kingdom?", 
-    	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-    	correctAnswer:"David Cameron",
-    	storedAnswer:"",
-    	answered:false}
-    	];
+    //questions
+    var q = [
+        {
+        question: "Who is the President of the United States today?", 
+        choices: ["George W. Bush", "John MCain", "Barack Obama", "Tony Blair"], 
+        correctAnswer: "Barack Obama"},
+        {
+        question: "Which of these is Javascript proper?", 
+        choices: ["<script></script>", "<script type='text/javascript'><script>", "</script>", "<script type='text/javascript'></script>"], 
+        correctAnswer:"<script type='text/javascript'></script>"},
+        {
+        question: "In 1822, who purposed and began developing the Difference Engine?", 
+        choices: ["Tommy Flowers", "John Vincent Atanasoff", "Charles Babbage", "J. Presper Eckert"], 
+        correctAnswer:"Charles Babbage"},
+        {
+        question: "In 2009, who created the Node.js platform that is built on top of Google Chromes V8 Engin?", 
+        choices: ["Urban Hafner", "Ryan Dahl", "Abe Fettig", "Joshaven Potter"], 
+        correctAnswer:"Ryan Dahl"},
+        {
+        question: "In Douglas Engelbarts 1968 presentation entitled, 'The Mother of All Demos', what entity do words makeup?", 
+        choices: ["Statement", "Command", "String", "List"], 
+        correctAnswer:"Statement"}
+        ];
+    //store answers
     var storedAnswers = [];
+    //log score
     var score = 0;
+    //global answer
     var answer ;
 
-$(document).ready(function () {
-		
+    //start quiz
     $("#start").on("click", function(event){
         event.preventDefault;
-        if(q.length > 0){
-    	$("#start").hide();
+        $("#start").hide();
         console.log("quiz started");
-    	$("#quizContainer").removeClass("hidden");
-        }
+        $("#quizContainer").removeClass("hidden");
     });
 
+	//populate the page with new question and answers
+    $("#question").text(q[0].question);
+    $("#A").text(q[0].choices[0]);
+    $("#B").text(q[0].choices[1]);
+    $("#C").text(q[0].choices[2]);
+    $("#D").text(q[0].choices[3]);	
 
+    //next question
     $("#A,#B,#C,#D").on("click", function(event){
         event.preventDefault;
-        if(q.length > 1){
-        /*!
-        *  next = get button user clicks
-        *  next = match selection to correct answer
-        *  next = store answer
-        *  next = update question to answered to prevent it from being asked again
-        *  next = get next unanswered question
-        *  next = go to next question
-        */ 
+        if(q.length >= 1){
+
         //get and store the users answer
         answer = $(this).text();
         storedAnswers.push(answer);
-        q.shift();
-        $("#question").text(q[0].question);
-                $("#A").text(q[0].choices[0]);
-                $("#B").text(q[0].choices[1]);
-                $("#C").text(q[0].choices[2]);
-                $("#D").text(q[0].choices[3]);
-        log("POPPED" + q.length);
-        //log(an);
         log("Answer = "+answer);
         log("Stored Answers = "+storedAnswers);
-        log("There are "+ q.length + " questions remaining");
-        } else {
-            //hide all the buttons and show end message
-        answer = $(this).text();
-        storedAnswers.push(answer);
-        q.shift();
-        //log("---------------");
-        //log("POPPED" + q.length);
-        //log(an);
-        log("Answer = "+answer);
-        log("Stored Answers = "+storedAnswers);
-        log("There are "+ q.length + " questions remaining");
-        log("Correct Answer = "+correctANswer);
-        log("The current score is : "+score);
-        q.shift();
-        log("---------------");
-        log("POPPED" + q.length);
-            $("#A,#B,#C,#D").hide();
-            $("#question").text("THE END");
-        }
-
         //get the correct answer 
             var correctANswer = q[0].correctAnswer;
             log("Correct Answer = "+correctANswer);
@@ -142,47 +83,41 @@ $(document).ready(function () {
                 score--; //subtract from score
                 log("The current score is : "+score);
             }
-
-            //remove question 
-            //log( q.shift() );//.pop();
-            //log("There are "+ q.length + " questions remaining");
-    });
-
-        //for(var i=0; i<q.length; i++){
-            //if(q.length > 0){
-
-                //populate new question and answers
-                $("#question").text(q[0].question);
+        //remove the question it's done
+        q.shift();
+        //get the next question
+        $("#question").text(q[0].question);
                 $("#A").text(q[0].choices[0]);
                 $("#B").text(q[0].choices[1]);
                 $("#C").text(q[0].choices[2]);
                 $("#D").text(q[0].choices[3]);
+        log("POPPED" + q.length);
+        //log(an);
+        log("There are "+ q.length + " questions remaining");
 
-//               //get the correct answer 
-//               var correctANswer = q[0].correctAnswer;
-//               log("Correct Answer = "+correctANswer);
-//               //compare users answer with correct answer
-//               if(answer == correctANswer){
-//                   score++; //add to score
-//                   log("The current score is : "+score);
-//               }else if(answer != correctANswer){
-//                   score--; //subtract from score
-//                   log("The current score is : "+score);
-//               }
+        } else {
 
-//               //remove question 
-//               //log( q.shift() );//.pop();
-//               log("There are "+ q.length + " questions remaining");
+        //hide all the buttons, calculate percent and show end message
+        //get the answer and store it
+        answer = $(this).text();
+        storedAnswers.push(answer);
+        //remove the final question
+        q.shift();
+        log("Answer = "+answer);
+        log("Stored Answers = "+storedAnswers);
+        log("There are "+ q.length + " questions remaining");
+        log("Correct Answer = "+correctANswer);
+        log("The current score is : "+score);
+        //hide the buttons
+        $("#A,#B,#C,#D").hide();
+        //calculate users final score
+        var percent = score / 5 * 100;
+        //show the users results
+        var display = storedAnswers.join("-");
+        $("#question").text("THE END" +"You scored "+percent+"% and Your answers " + display);
+        }
 
-//               
-
-//           }else {
-//               //hide all the buttons and show end message
-//               $("#A,#B,#C,#D").hide();
-//               $("#question").text("THE END");
-//           }
-//       //}
-//
-    
-
+        //end
+    });
+                
 });
