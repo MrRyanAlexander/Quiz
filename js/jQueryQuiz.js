6eecce1145287f195b582cac980593034a9a1696
MrRyanAlexander <lazyguymedia@gmail.com>
2013-10-16 17:26:33 -0700

    updated
    * implemented cross browser compatibility update
    * implemented next & back buttons
    * implemented number of tries until the end
    * implemented forced questions

--- js/jQueryQuiz.js
+++ js/jQueryQuiz.js
@@ -1,6 +1,32 @@
+/*!
+* created by: Ryan Alexander on Oct 5, 2013
+* contact: lazyguymedia@gmail.com
+* website: ryan-alexander.com
+*
+* created quiz
+* implemented results page
+* inplemented localStorage
+* implemented cross browser compatability update
+* implemented next & back buttons
+* implemented number of trys until the end
+* implemented forced questions
+* need - implemented Json file, deleted JS object
+* need - implemented User Signup and Login Authentication
+* need - implemented Twitter Bootstrap
+* need ? 
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
 $(document).ready(function() {
 
-	//questions
+	//questions //futur JSON
     var q = [
         {
         question: "Who is the President of the United States today?", 
@@ -26,12 +52,13 @@ $(document).ready(function() {
         question: "In Douglas Engelbarts 1968 presentation entitled, 'The Mother of All Demos', what entity do words makeup?", 
         choices: ["Statement", "Command", "String", "List"], 
         correctAnswer:"Statement",
-        userAnswer: []},
+        userAnswer: []}
         ];
-
 	var currentQuestion = 0;
 	var score = 0;
 	var numQuestions = q.length;
+	var trys = 0;
+	var trysLeft = 3;
 
 	//hide on load
 	$('#results, .quizContainer, #restart').hide();
@@ -42,69 +69,75 @@ $(document).ready(function() {
 		$(this).fadeOut(100);
 		$('.quizContainer').fadeIn('slow');
 		$('#back, #submit').hide();
-		loadData();
-		
+		loadData();	
 	});
 
 	//back button
 	$("#back").click(function(e){
 		e.preventDefault();
 		$('.quizContainer').hide().fadeIn('slow');
-		//prevent position from going negative
+		//prevent position from going negative. Change to 1 to hide (this) first. 
 		if(currentQuestion == 0){
 			//do something
-			$('#back').fadeOut('slow');
-			alert("You're at the beginning, answer some questions and click next to move forward!");
+			$(this).fadeOut('slow');
+			alert("You're at the beginning, answer some questions or change some answer and click next to move forward! You have "+trysLeft+" trys remaining!");
 		}else{
 			storeData();
 			currentQuestion--;
 			loadData();
 			$('#next').fadeIn('slow');
-		log("Clicked back and the score is: " + score);
-		log("Clicked back and the question is: " + currentQuestion);
-		}
-		
+		}	
 	});
 
-    //next button
-	$("#next").click(function(e){
-		e.preventDefault();
-		$('.quizContainer').hide().fadeIn('slow');
-		//prevent position from going beyond questions
-		if(currentQuestion == numQuestions -1){
-			storeData();
-			//alert("You've reached the end of the quiz. Congrats!");
-			$('#next').fadeOut('slow');
-			$('#submit').fadeIn('slow');
-		}else{			
-			storeData();
-			currentQuestion++;
-			loadData();
-			$('#back').fadeIn('slow');
-		log("Clicked next and the score is: " + score);
-		log("Clicked next and the question is: " + currentQuestion);
-		}
-		
-	});
 
-	//hidden submit button
-	$('#submit').click(function(e){
-		e.preventDefault();
-		$('.quizContainer').fadeOut('fast');
-		$('.buttons').fadeOut('fast');
-		$('#results').fadeIn('slow');
-		results();
-		$('#restart').fadeIn('slow');
-	});
+    $("#next").click(function(e){
+        e.preventDefault();
+        //force user to answer question
+        if($('input[name=choices]:checked').length > 0){
 
+	        $('.quizContainer').hide().fadeIn('slow');
+	        //prevent position from going beyond questions
+	            if(currentQuestion == numQuestions -1){
+                	storeData();
+                	$('#next, #back, .question, .choiceList').fadeOut('slow');
+                    trys++;//only allow 3 trys to complete quiz
+                        if(trys < 3){
+							results();
+							$('#results, #restart').fadeIn('slow');
+						}else{
+							results();
+							$('#results').fadeIn('slow');
+						}
+                        trysLeft--;
+                        alert("You've reached the end of this try. You have "+ trysLeft + " trys remaining!");
+	            }else{                        
+	                storeData();
+	                currentQuestion++;
+	                loadData();
+	                $('#back').fadeIn('slow');
+	                }
+        }else{
+        	alert("Oops! You forgot to answer this question.");
+        }   
+    });
+
+	//hidden restart button
 	$('#restart').click(function(e){
 		e.preventDefault();
+		//reset();
 		currentQuestion = 0;
 		$('#back, #submit, #restart, #results').hide();
-		$('#next, .quizContainer').fadeIn('slow');
+		$('.question, .choiceList, #next').fadeIn('slow');
 		loadData();
 	});
 
+	function reset(){
+		var num = document.getElementById('1');
+		var answer = document.getElementById('2');
+		num.parentNode.removeChild(num);
+		answer.parentNode.removeChild(answer);
+	}
+	//generate results
 	function results(){
 		for(i=0; i<numQuestions; i++){
 			log(i + " : " + q[i].userAnswer);
@@ -116,8 +149,8 @@ $(document).ready(function() {
 
 		var tdNum = document.createElement('td');
 		var tdAnswer = document.createElement('td');
-
-		var num = document.createTextNode(i);
+		var qNum = i + 1;
+		var num = document.createTextNode("Question : " + qNum);
 		var answer = document.createTextNode(q[i].userAnswer);
 
 		tdNum.appendChild(num);
@@ -144,11 +177,11 @@ $(document).ready(function() {
 
 
 			// Set the questionClass text to the current question
-		    $('.question').hide().fadeIn('slow');
+		    $('.question').fadeIn('slow');
 		    $('.question').text(question);
 		
 			//Fade the buttons with question
-			$('.buttons').hide().fadeIn('slow');
+			$('.buttons').fadeIn('slow');
 			
 		    // Remove all current <li> elements (if any)
 		    $('.choiceList').find("li").remove();
@@ -156,28 +189,37 @@ $(document).ready(function() {
 		    var i, choice, userAnswer, localAnswer, localVal, tag;
 		    for (i = 0; i < numChoices; i ++) {
 		        choice = q[currentQuestion].choices[i];
-		        userAnswer = q[currentQuestion].userAnswer; 
-		        localAnswer = localStorage.getItem("answer " + currentQuestion);
-		        localVal = localStorage.getItem("value " + currentQuestion);
-		        log("Local Answer: " + localAnswer); 
-		        log("Local Value: " + localVal);
+
+		        if(localStorage.getItem("answer " + currentQuestion) == null || 
+		           localStorage.getItem("answer " + currentQuestion) == undefined ){
+
+		        	localAnswer = "not answered";
+		        	localVal = "no value";
+		        	log("Local Answer: " + localAnswer); 
+		        	log("Local Value: " + localVal);
+		    	}else{
+		    		localAnswer = localStorage.getItem("answer " + currentQuestion);
+		    		localVal = localStorage.getItem("value " + currentQuestion);
+		    		log("Local Answer: " + localAnswer); 
+		        	log("Local Value: " + localVal);
+		    	}
 		
 		        // If we already have a user answer for this choice then check the radio button
 		        if ( i == localVal ) {
-		            tag = '<li><input type="radio" value=' + localVal + ' id=' + localVal + ' checked=true name="choices" ><label for=' + localVal + '>'+choice+'</label</li>';
+		            tag = '<li><input type="radio" value=' + localVal + ' id=' + localVal + ' checked=true name="choices" ><label for=' + localVal + '>'+choice+'</label></li>';
 		        	//break;
 		        	log("radio checked");
 		        } else {
-		            tag = '<li><input type="radio" value=' + i + ' id=' + i + ' name="choices" ><label for=' + i + '>'+choice+'</label</li>';
+		            tag = '<li><input type="radio" value=' + i + ' id=' + i + ' name="choices" ><label for=' + i + '>'+choice+'</label></li>';
 		        	//break;
 		        	log("radio not checked");
 		        }
 		        //log(tag);
-		        $(tag).appendTo('.choiceList').hide().fadeIn('slow');
-	    	} 
-			
+		        $(tag).appendTo('.choiceList').fadeIn('slow');
+	    	} 		
     }
     	
+    //store data locally	
     function storeData(){
     	if($('input[name=choices]:checked').length > 0){
 				var txt = $('input:radio:checked + label').text(); //checked text 
@@ -200,6 +242,7 @@ $(document).ready(function() {
 		       	localStorage.setItem("value " + currentQuestion, val);
 		    }
     }
+
     //create log 
     function log(t){
         console.log(t);
